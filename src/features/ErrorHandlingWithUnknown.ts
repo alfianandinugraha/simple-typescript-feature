5db9279cfdd60939f8da16775fc27d4231ac26c5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-28 12:17:16 +0700

    feat(features): add another error on ErrorHandlingWithUnknown

--- src/features/ErrorHandlingWithUnknown.ts
+++ src/features/ErrorHandlingWithUnknown.ts
@@ -1,3 +1,32 @@
+class HttpError<T> {
+  code: number;
+  message: string;
+  body?: T;
+
+  constructor(code: number, message: string, body?: T) {
+    this.code = code;
+    this.message = message;
+    this.body = body;
+  }
+}
+
+function storeUser(name: string) {
+  if (name) return "Success to store user !";
+  throw new HttpError(401, "Please insert name");
+}
+
+try {
+  const result = storeUser("");
+  console.log(result);
+} catch (err: unknown) {
+  if (err instanceof HttpError) {
+    console.error("HTTP Error");
+    console.error(err);
+  } else {
+    console.log("Something wrong");
+  }
+}
+
 function fetchDummy(length: number) {
   if (length >= 10) return "Success !";
   throw new Error("This is bad !");
