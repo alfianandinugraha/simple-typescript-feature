ca5a233b7d276ee2aeb1be80b1b7e889e2c5bf30
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-10-28 13:47:50 +0700

    feat(features): add new error class: ParamError

--- src/features/ErrorHandlingWithUnknown.ts
+++ src/features/ErrorHandlingWithUnknown.ts
@@ -10,9 +10,22 @@ class HttpError<T> {
   }
 }
 
+class ParamError {
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+
+  static validate(err: any) {
+    return err instanceof ParamError;
+  }
+}
+
 function storeUser(name: string) {
-  if (name) return "Success to store user !";
-  throw new HttpError(401, "Please insert name");
+  if (!name) throw new ParamError("Please input name");
+  if (name.length >= 5) throw new HttpError(401, "Name is longer");
+  return "Success to store user !";
 }
 
 try {
@@ -21,6 +34,9 @@ try {
 } catch (err: unknown) {
   if (err instanceof HttpError) {
     console.error("HTTP Error");
+    console.error(err);
+  } else if (ParamError.validate(err)) {
+    console.error("Param error");
     console.error(err);
   } else {
     console.log("Something wrong");
